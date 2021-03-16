ad692b55d5f4cea782e9700f53d00a099b273cbd
Ben Allen <external@benallen.info>
2021-03-16 12:02:51 +0000

    Added proxy install

--- dev/install/run.sh
+++ dev/install/run.sh
@@ -3,6 +3,7 @@ export ROOT_PATH=$(pwd)
 # Install clients
 yarn --cwd "$ROOT_PATH/clients/web" install
 yarn --cwd "$ROOT_PATH/clients/app" install
+yarn --cwd "$ROOT_PATH/clients/proxy" install
 
 # Install services
 yarn --cwd "$ROOT_PATH/services/api" install
