d85919a3b2e1d076bbc7d47ae34aae59789da5f3
Ben Allen <external@benallen.info>
2021-03-22 13:32:49 +0000

    Skip cert creation if on CI

--- clients/proxy/dev/certs/run.sh
+++ clients/proxy/dev/certs/run.sh
@@ -1,4 +1,9 @@
 export ROOT_PATH=$(pwd)
 
-
+if [ -z "$CI" ]
+then
+echo "Creating cert...."
 mkcert -cert-file "$ROOT_PATH/omnea.local.pem" -key-file "$ROOT_PATH/omnea.local-key.pem" omnea.local admin.omnea.local api.omnea.local docs.omnea.local hosted.omnea.local localhost
+else
+echo "Skipping cert...."
+fi
